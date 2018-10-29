01e549007236f3853cd6cef2d922b0e166918b20
Sade <smithsade13@gmail.com>
2018-10-28 19:13:07 -0500

    refactor

--- lambda/custom/config-example.js
+++ lambda/custom/config-example.js
@@ -1,8 +1,8 @@
 'use strict';
 
 const Config = {
-    'API_KEY': 'ee1c97f9b7a5442ea271d0e6072ea800',
-    'STATION_CODE': '41260'
+    'API_KEY': '',
+    'STATION_CODE': ''
 };
 
 module.exports = Config;
